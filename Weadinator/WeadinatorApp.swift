9e24186fe2c8b136816521ee9aa635276326fc84
Cjungwo <jwoo516@naver.com>
2024-10-09 15:07:02 +1100

    Initial Commit

---
+++ Weadinator/WeadinatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeadinatorApp.swift
+//  Weadinator
+//
+//  Created by JungWoo Choi on 9/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct WeadinatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
