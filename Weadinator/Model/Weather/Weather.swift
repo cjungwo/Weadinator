3036c98664833d730d6f786a452803fa4732bfce
Cjungwo <jwoo516@naver.com>
2024-10-18 13:49:42 +1100

    #14 - update clothing, weather swiftdata model

--- Weadinator/Model/Weather/Weather.swift
+++ Weadinator/Model/Weather/Weather.swift
@@ -11,28 +11,30 @@ import SwiftData
 
 @Model
 class Weather {
-    #Unique<Weather>([\.date])
-    
-    var date: Date
-    var location: CLLocation
-    var temperatureHigh: Double
-    var temperatureLow: Double
-    var condition: WeatherCondition
-    var precipitation: Double
-    
-    init(
-        date: Date,
-        location: CLLocation,
-        temperatureHigh: Double,
-        temperatureLow: Double,
-        condition: WeatherCondition,
-        precipitation: Double
-    ) {
-        self.date = date
-        self.location = location
-        self.temperatureHigh = temperatureHigh
-        self.temperatureLow = temperatureLow
-        self.condition = condition
-        self.precipitation = precipitation
-    }
+  #Unique<Weather>([\.date])
+
+  var date: Date
+  var longitube: Double
+  var latitube: Double
+  var temperatureHigh: Double
+  var temperatureLow: Double
+  var condition: WeatherCondition
+  var precipitation: Double
+
+  init(
+    date: Date,
+    location: CLLocation,
+    temperatureHigh: Double,
+    temperatureLow: Double,
+    condition: WeatherCondition,
+    precipitation: Double
+  ) {
+    self.date = date
+    self.longitube = location.coordinate.longitude
+    self.latitube = location.coordinate.latitude
+    self.temperatureHigh = temperatureHigh
+    self.temperatureLow = temperatureLow
+    self.condition = condition
+    self.precipitation = precipitation
+  }
 }
