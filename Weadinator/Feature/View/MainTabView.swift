6a5960971e35918d6c8542b475caa02dd1ce493a
Cjungwo <jwoo516@naver.com>
2024-10-18 14:07:12 +1100

    #19 - Create MainTabView

---
+++ Weadinator/Feature/View/MainTabView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  Weadinator
+//
+//  Created by JungWoo Choi on 9/10/2024.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+  var body: some View {
+    TabView {
+      Tab {
+        Text("HomeView")
+      } label: {
+        Label("Home", systemImage: "cloud.sun")
+      }
+
+      Tab {
+        Text("WardrobeView")
+      } label: {
+        Label("Wardrobe", systemImage: "door.french.closed")
+      }
+    }
+  }
+}
+
+#Preview {
+  MainTabView()
+}
