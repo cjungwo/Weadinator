2a3db91c1f943ce4f90087ae96826d37fcaecebf
Cjungwo <jwoo516@naver.com>
2024-10-22 16:11:33 +1100

    #28 - add ClothingManager to manage clothing model

--- Weadinator/Core/Service/ClothingManager.swift
+++ Weadinator/Core/Service/ClothingManager.swift
@@ -5,8 +5,16 @@
 //  Created by JungWoo Choi on 18/10/2024.
 //
 
-import Foundation
+import SwiftUI
+import PhotosUI
 
 class ClothingManager: ObservableObject {
+  @Published var selectedImage: PhotosPickerItem?
+  @Published var selectedImageData: Data?
+  @Published var clothing = Clothing()
+  @Published var title: String = ""
 
+  func addBtnTapped() {
+    
+  }
 }
