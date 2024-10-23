82eabd42737aebe9b2848b0afef4c94352ae666a
Cjungwo <jwoo516@naver.com>
2024-10-23 11:44:02 +1100

    #28 - add Text extension for design guide

---
+++ Weadinator/Core/Extension/Text+Ext.swift
@@ -0,0 +1,25 @@
+//
+//  Text+Ext.swift
+//  Weadinator
+//
+//  Created by JungWoo Choi on 23/10/2024.
+//
+
+import SwiftUI
+
+extension Text {
+  var title3Text: some View {
+    self
+    .font(.system(size: 22, weight: .medium))
+  }
+
+  var labelText: some View {
+    self
+      .font(.system(size: 18, weight: .bold))
+  }
+
+  var btnText: some View {
+    self
+      .font(.system(size: 16, weight: .medium))
+  }
+}
