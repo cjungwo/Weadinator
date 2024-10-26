8252e143dd4cb2b36ff81faac7d59e6e9e9e72b9
Cjungwo <jwoo516@naver.com>
2024-10-27 10:01:11 +1100

    #38 - add Testing Framework for unit testing

---
+++ Text/ClothingManagerTest.swift
@@ -0,0 +1,16 @@
+//
+//  ClothingManagerTest.swift
+//  Weadinator
+//
+//  Created by JungWoo Choi on 27/10/2024.
+//
+
+import Testing
+
+struct ClothingManagerTest {
+
+    @Test func <#test function name#>() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
