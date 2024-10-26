cc4fef582b3bbeb176c178112e76f0280c5696c5
Cjungwo <jwoo516@naver.com>
2024-10-26 14:01:06 +1100

    #36 - add Coordinator to recommand clothing style

--- Weadinator/Core/Service/Coordinator.swift
+++ Weadinator/Core/Service/Coordinator.swift
@@ -6,3 +6,89 @@
 //
 
 import Foundation
+
+class Coordinator: ObservableObject {
+  let tempHigh: Double
+  let tempLow: Double
+  let clothingList: [Clothing]
+
+  var tempAvg: Double {
+    (tempHigh + tempLow)/2
+  }
+
+  init(
+    tempHigh: Double,
+    tempLow: Double,
+    clothingList: [Clothing]
+  ) {
+    self.tempHigh = tempHigh
+    self.tempLow = tempLow
+    self.clothingList = clothingList
+  }
+
+  func calcWarmthLevelFromTempAvg() -> WarmthLevel {
+    var warmthLevel: WarmthLevel = .cold
+
+    switch tempAvg {
+    case ..<0:
+      warmthLevel = WarmthLevel.cold
+    case 0..<10:
+      warmthLevel = WarmthLevel.cool
+    case 10..<20:
+      warmthLevel = WarmthLevel.moderate
+    case 20..<30:
+      warmthLevel = WarmthLevel.warm
+    case 30...:
+      warmthLevel = WarmthLevel.hot
+    default:
+      warmthLevel = WarmthLevel.cold
+    }
+
+    return warmthLevel
+  }
+
+  func filterClothingListByWarmthLevel(clothingList: [Clothing], warmthLevel: WarmthLevel) -> [Clothing] {
+    var newList = [Clothing]()
+
+    newList = clothingList.filter { $0.warmthLevel == warmthLevel }
+
+    return newList
+  }
+
+  func filterClothingListByClothingType(_ clothingType: ClothingType, from clothingList: [Clothing]) -> [Clothing] {
+    var newList = [Clothing]()
+
+    newList = clothingList.filter { $0.clothingType == clothingType }
+
+    return newList
+  }
+
+  func getRandomClothing(clothingList: [Clothing], clothingType: ClothingType) -> Clothing? {
+    let filteredListByWarmthLevel = filterClothingListByWarmthLevel(clothingList: clothingList, warmthLevel: calcWarmthLevelFromTempAvg())
+    let filteredListByClothingType = filterClothingListByClothingType(clothingType, from: filteredListByWarmthLevel)
+
+    let randomClothing: Clothing? = filteredListByClothingType.randomElement()
+
+    return randomClothing
+  }
+
+  func generateStyle() -> [Clothing?] {
+    var newClothingList = [Clothing?]() // ClothingList [Jacket, Shirt, Trousers, ... ] -> Style
+
+    for type in ClothingType.allCases {
+      newClothingList.append(getRandomClothing(clothingList: self.clothingList, clothingType: type))
+    }
+
+    return newClothingList
+  }
+
+  func generateStyleList() -> [[Clothing?]] {
+    var newStyleList = [[Clothing?]]() // StyleSet List
+
+    for _ in 0..<4 {
+      newStyleList.append(generateStyle())
+    }
+
+    return newStyleList
+  }
+}
