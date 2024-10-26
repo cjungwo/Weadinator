bb4469e8bf92bc7f7ab323a4007c86ed98e30a66
Cjungwo <jwoo516@naver.com>
2024-10-27 10:20:42 +1100

    #38 - set info of project

--- WeadinatorTests/ClothingManagerTests.swift
+++ WeadinatorTests/ClothingManagerTests.swift
@@ -1,13 +1,13 @@
 //
-//  ClothingManagerTest.swift
-//  Weadinator
+//  ClothingManagerTests.swift
+//  WeadinatorTests
 //
 //  Created by JungWoo Choi on 27/10/2024.
 //
 
 import Testing
 
-struct ClothingManagerTest {
+struct ClothingManagerTests {
 
     @Test func <#test function name#>() async throws {
         // Write your test here and use APIs like `#expect(...)` to check expected conditions.
