f6ad68bbdc638a6ad77ed9eb55b1ecfb95b6b86e
Cjungwo <jwoo516@naver.com>
2024-10-28 16:34:26 +1100

    #38 - [test] add CoordinatorTests

--- WeadinatorTests/CoordinatorTests.swift
+++ WeadinatorTests/CoordinatorTests.swift
@@ -6,11 +6,34 @@
 //
 
 import Testing
+@testable import Weadinator
 
+@Suite("Coordinator Tests")
 struct CoordinatorTests {
 
-    @Test func <#test function name#>() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-    }
+  let sut: Coordinator = Coordinator(tempHigh: 5, tempLow: 13, clothingList: MockData.clothingList)
+
+
+  @Test("Return warmth level from avg temp") func givenNothing_whencalcWarmthLevel_thenReturnWarmthLevelCorrectly() async throws {
+    // given
+    let expected: WarmthLevel = .cool
+
+    // when
+    let result: WarmthLevel = sut.calcWarmthLevelFromTempAvg()
+
+    // then
+    #expect(result == expected)
+  }
+
+  @Test("Return warmth level from avg temp") func givenWarmthLevel_whencalcWarmthLevel_thenReturnWarmthLevelCorrectly() async throws {
+    // given
+    let expected: WarmthLevel = .cool
+
+    // when
+    let result: WarmthLevel = sut.calcWarmthLevelFromTempAvg()
+
+    // then
+    #expect(result == expected)
+  }
 
 }
