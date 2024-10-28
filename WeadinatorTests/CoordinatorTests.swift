d7f73c04d3f922a9aa0e1d64f064a9fb2dd071fc
Cjungwo <jwoo516@naver.com>
2024-10-28 17:58:50 +1100

    #38 - add CoordinatorTests

--- WeadinatorTests/CoordinatorTests.swift
+++ WeadinatorTests/CoordinatorTests.swift
@@ -14,7 +14,7 @@ struct CoordinatorTests {
   let sut: Coordinator = Coordinator(tempHigh: 5, tempLow: 13, clothingList: MockData.clothingList)
 
 
-  @Test("Return warmth level from avg temp") func givenNothing_whencalcWarmthLevel_thenReturnWarmthLevelCorrectly() async throws {
+  @Test("Return warmth level from temp avg") func givenNothing_whenCalcWarmthLevelByTempAvg_thenReturnWarmthLevelCorrectly() async throws {
     // given
     let expected: WarmthLevel = .cool
 
@@ -25,15 +25,61 @@ struct CoordinatorTests {
     #expect(result == expected)
   }
 
-  @Test("Return warmth level from avg temp") func givenWarmthLevel_whencalcWarmthLevel_thenReturnWarmthLevelCorrectly() async throws {
+  @Test("Filtering clothng list by warmth level") func givenWarmthLevel_whenFilterClothingListByWarmthLevel_thenReturnNewList() async throws {
     // given
-    let expected: WarmthLevel = .cool
+    let warmthLevel: WarmthLevel = .cool
 
     // when
-    let result: WarmthLevel = sut.calcWarmthLevelFromTempAvg()
+    let newList: [Clothing] = sut.filterClothingListByWarmthLevel(clothingList: MockData.clothingList, warmthLevel: .cool)
 
     // then
-    #expect(result == expected)
+    #expect((newList.first?.warmthLevel ?? .cool) == warmthLevel)
+    #expect((newList.last?.warmthLevel ?? .cool) == warmthLevel)
   }
+
+  @Test("Filtering clothng list by clothing type") func givenClothingType_whenFilterClothingListByClothingType_thenReturnNewList() async throws {
+    // given
+    let clothingType: ClothingType = .shoes
+
+    // when
+    let newList: [Clothing] = sut.filterClothingListByClothingType(.shoes, from: MockData.clothingList)
+
+    // then
+    #expect((newList.first?.clothingType ?? .shoes) == clothingType)
+    #expect((newList.last?.clothingType ?? .shoes) == clothingType)
+  }
+
+  @Test("Get random clothing successfully") func givenClothingListAndClothingType_whenGetRandomClothing_thenReturnRandomClothing() async throws {
+    // given
+    let clothingType: ClothingType = .shoes
+
+    // when
+    let randomClothing: Clothing? = sut.getRandomClothing(clothingList: MockData.clothingList, clothingType: clothingType)
+
+    // then
+    #expect(randomClothing?.clothingType ?? .shoes == clothingType)
+  }
+
+  @Test("Generate style successfully") func givenNothing_whenGenerateStyle_thenReturnClothingListAsStyle() async throws {
+    // given
+
+    // when
+    let style: [Clothing?] = sut.generateStyle()
+
+    // then
+    #expect(style.count == ClothingType.allCases.count)
+  }
+
+  @Test("Generate style list successfully") func givenNothing_whenGenerateStyleList_thenReturnStyleList() async throws {
+    // given
+
+    // when
+    let styleList: [[Clothing?]] = sut.generateStyleList()
+
+    // then
+    #expect(styleList.count == 4)
+    #expect(styleList.first?.count == 5)
+  }
+
 
 }
