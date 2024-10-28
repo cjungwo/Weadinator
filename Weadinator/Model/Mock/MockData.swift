161309c1986696a43d959160a076a4bdd1777020
Cjungwo <jwoo516@naver.com>
2024-10-28 16:32:16 +1100

    #38 - add MockData for test Coordinator

---
+++ Weadinator/Model/Mock/MockData.swift
@@ -0,0 +1,33 @@
+//
+//  MockData.swift
+//  Weadinator
+//
+//  Created by JungWoo Choi on 28/10/2024.
+//
+
+import Foundation
+
+class MockData {
+  static var clothingList: [Clothing] = [
+    .init(title: "Jacket1", clothingType: .jacket, warmthLevel: .cool, clothingColor: .beige),
+    .init(title: "Jacket2", clothingType: .jacket, warmthLevel: .cool, clothingColor: .green),
+    .init(title: "Jacket3", clothingType: .jacket, warmthLevel: .cool, clothingColor: .blue),
+    .init(title: "Jacket4", clothingType: .jacket, warmthLevel: .cool, clothingColor: .black),
+    .init(title: "Shirt1", clothingType: .shirt, warmthLevel: .cool, clothingColor: .white),
+    .init(title: "Shirt2", clothingType: .shirt, warmthLevel: .cool, clothingColor: .gray),
+    .init(title: "Shirt3", clothingType: .shirt, warmthLevel: .cool, clothingColor: .yellow),
+    .init(title: "Shirt4", clothingType: .shirt, warmthLevel: .cool, clothingColor: .red),
+    .init(title: "Trousers1", clothingType: .trousers, warmthLevel: .cool, clothingColor: .black),
+    .init(title: "Trousers2", clothingType: .trousers, warmthLevel: .cool, clothingColor: .blue),
+    .init(title: "Trousers3", clothingType: .trousers, warmthLevel: .cool, clothingColor: .ivory),
+    .init(title: "Trousers4", clothingType: .trousers, warmthLevel: .cool, clothingColor: .beige),
+    .init(title: "Shoes1", clothingType: .shoes, warmthLevel: .cool, clothingColor: .black),
+    .init(title: "Shoes2", clothingType: .shoes, warmthLevel: .cool, clothingColor: .white),
+    .init(title: "Shoes3", clothingType: .shoes, warmthLevel: .cool, clothingColor: .red),
+    .init(title: "Shoes4", clothingType: .shoes, warmthLevel: .cool, clothingColor: .beige),
+    .init(title: "Accessory1", clothingType: .accessories, warmthLevel: .cool, clothingColor: .white),
+    .init(title: "Accessory2", clothingType: .accessories, warmthLevel: .cool, clothingColor: .white),
+    .init(title: "Accessory3", clothingType: .accessories, warmthLevel: .cool, clothingColor: .green),
+    .init(title: "Accessory4", clothingType: .accessories, warmthLevel: .cool, clothingColor: .red)
+  ]
+}
