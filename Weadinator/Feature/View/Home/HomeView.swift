e038d89314e437005fce567b245bed4b62ce1f8e
KoreanJoey <dolha4166@gmail.com>
2024-10-28 15:38:18 +1100

    #36 - Update RecoomedationClothingView to display style, and LocationManager to show location name

--- Weadinator/Feature/View/Home/HomeView.swift
+++ Weadinator/Feature/View/Home/HomeView.swift
@@ -22,15 +22,16 @@ struct HomeView: View {
             // WeatherShowingView()
             HStack{
                 WeatherIconView(iconUrl: "https://openweathermap.org/img/wn/\(weatherManager.weather?.iconCode ?? "01d")@2x.png", size: 100)
-                VStack{
-                    Text("Location")
+                Text("\(Int(weatherManager.weather?.currentTemp ?? 0))°")
+                    .font(.system(size: 40))
+                    .foregroundColor(.white)
+
+                VStack(alignment: .leading){
+                    Text(locationManager.locationName ?? "Location")
                         .font(.headline)
                         .foregroundColor(.white)
                     HStack {
-                        Text("\(Int(weatherManager.weather?.currentTemp ?? 0))°")
-                            .font(.system(size: 40))
-                            .foregroundColor(.white)
-                        VStack{
+                                                VStack{
                             Text(weatherManager.weather?.description ?? "Unknown")
                                 .font(.subheadline)
                                 .foregroundColor(.white)
@@ -132,6 +133,156 @@ private struct EmptyClothingView: View {
     }
 }
 
+struct RecommendationClothingView: View {
+    let styleList: [[Clothing?]]
+    
+    var body: some View {
+        TabView {
+            ForEach(0..<styleList.count, id: \.self) { index in
+                RecommendationClothingListView(style: styleList[index])
+            }
+        }
+        .tabViewStyle(PageTabViewStyle())
+        .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
+    }
+}
+
+
+struct RecommendationClothingListView: View {
+    let style: [Clothing?]  // One Style from clothing type
+    
+    var body: some View {
+        HStack {
+            // Left: jacket
+            VStack {
+                Rectangle()
+                    .foregroundColor(.clear)
+                    .frame(width: 100, height: 160)
+                    .background(
+                        Group {
+                            if let clothing = style[safe: 0] {
+                                clothingImage(for: clothing, alter: "jacket")
+                            }
+                        }
+                    )
+            }
+            // Middle: shirts, trousers, shoes
+            VStack {
+                Rectangle()
+                    .foregroundColor(.clear)
+                    .frame(width: 100, height: 160)
+                    .background(
+                        Group {
+                            if let clothing = style[safe: 1] {
+                                clothingImage(for: clothing, alter: "tshirt")
+                            }
+                        }
+                    )
+                
+                Rectangle()
+                    .foregroundColor(.clear)
+                    .frame(width: 100, height: 160)
+                    .background(
+                        Group {
+                            if let clothing = style[safe: 2] {
+                                clothingImage(for: clothing, alter: "hanger")
+                            }
+                        }
+                    )
+                
+                Rectangle()
+                    .foregroundColor(.clear)
+                    .frame(width: 100, height: 70)
+                    .background(
+                        Group {
+                            if let clothing = style[safe: 3] {
+                                clothingImage(for: clothing, alter: "shoe")
+                            }
+                        }
+                    )
+            }
+            // Right: bag
+            VStack {
+                Rectangle()
+                    .foregroundColor(.clear)
+                    .frame(width: 100, height: 160)
+                    .background(
+                        Group {
+                            if let clothing = style[safe: 4] {
+                                clothingImage(for: clothing, alter: "bag")
+                            }
+                        }
+                    )
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(CustomColor.color3)
+        .cornerRadius(10)
+        .padding()
+    }
+    
+    // MARK: - Helper Method
+    @ViewBuilder
+    private func clothingImage(for clothing: Clothing?, alter: String = "photo") -> some View {
+        if let clothing = clothing, let imageData = clothing.clothingImage, let uiImage = UIImage(data: imageData) {
+            Image(uiImage: uiImage)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .clipped()
+        } else {
+            Image(systemName: alter)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .foregroundColor(.gray)
+        }
+    }
+}
+
+extension Collection {
+    subscript(safe index: Index) -> Element? {
+        return indices.contains(index) ? self[index] : nil
+    }
+}
+
+//MARK: WeatherIconView
+private struct WeatherIconView: View {
+    var iconUrl: String
+    var size: CGFloat
+    
+    fileprivate var body: some View {
+        AsyncImage(url: URL(string: iconUrl)) { phase in
+            switch phase {
+            case .empty:
+                // Placeholder while loading
+                ProgressView()
+                    .frame(width: size, height: size)
+            case .success(let image):
+                // Display the fetched image
+                image
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: size, height: size)
+            case .failure:
+                // Display an error image or icon
+                ProgressView()
+                    .frame(width: size, height: size)
+                //                Image(systemName: "exclamationmark.triangle")
+                //                    .resizable()
+                //                    .scaledToFit()
+                //                    .frame(width: size, height: size)
+                //                    .foregroundColor(.red)
+            @unknown default:
+                // Fallback case
+                Image(systemName: "questionmark")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: size, height: size)
+                    .foregroundColor(.gray)
+            }
+        }
+    }
+}
+
 ////MARK: RecommendationClothingView
 //private struct RecommendationClothingView: View {
 //    fileprivate var body: some View {
@@ -208,183 +359,6 @@ private struct EmptyClothingView: View {
 //        .padding()
 //    }
 //}
-
-struct RecommendationClothingView: View {
-    let styleList: [[Clothing?]]
-    
-    var body: some View {
-        TabView {
-            ForEach(0..<styleList.count, id: \.self) { index in
-                RecommendationClothingListView(style: styleList[index])
-            }
-        }
-        .tabViewStyle(PageTabViewStyle())
-        .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
-    }
-}
-
-
-struct RecommendationClothingListView: View {
-    let style: [Clothing?]  // One Style from clothing type
-    
-    var body: some View {
-        HStack {
-            // Left: jacket
-            VStack {
-                Rectangle()
-                    .foregroundColor(.clear)
-                    .frame(width: 100, height: 160)
-                    .background(
-                        Group {
-                            if let clothing = style[safe: 0] {
-                                clothingImage(for: clothing)
-                            } else {
-                                Image(systemName: "jacket")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .foregroundColor(.gray)
-                            }
-                        }
-                    )
-            }
-            // Middle: shirts, trousers, shoes
-            VStack {
-                Rectangle()
-                    .foregroundColor(.clear)
-                    .frame(width: 100, height: 160)
-                    .background(
-                        Group {
-                            if let clothing = style[safe: 1] {
-                                clothingImage(for: clothing)
-                            } else {
-                                Image(systemName: "tshirt")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .foregroundColor(.gray)
-                            }
-                        }
-                    )
-                
-                Rectangle()
-                    .foregroundColor(.clear)
-                    .frame(width: 100, height: 160)
-                    .background(
-                        Group {
-                            if let clothing = style[safe: 2] {
-                                clothingImage(for: clothing)
-                            } else {
-                                Image(systemName: "hanger")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .foregroundColor(.gray)
-                            }
-                        }
-                    )
-                
-                Rectangle()
-                    .foregroundColor(.clear)
-                    .frame(width: 100, height: 70)
-                    .background(
-                        Group {
-                            if let clothing = style[safe: 3] {
-                                clothingImage(for: clothing)
-                            } else {
-                                Image(systemName: "shoe")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .foregroundColor(.gray)
-                            }
-                        }
-                    )
-            }
-            // Right: bag
-            VStack {
-                Rectangle()
-                    .foregroundColor(.clear)
-                    .frame(width: 100, height: 160)
-                    .background(
-                        Group {
-                            if let clothing = style[safe: 4] {
-                                clothingImage(for: clothing)
-                            } else {
-                                Image(systemName: "bag")
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fit)
-                                    .foregroundColor(.gray)
-                            }
-                        }
-                    )
-            }
-        }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .background(CustomColor.color3)
-        .cornerRadius(10)
-        .padding()
-    }
-    
-    // MARK: - Helper Method
-    @ViewBuilder
-    private func clothingImage(for clothing: Clothing?) -> some View {
-        if let clothing = clothing, let imageData = clothing.clothingImage, let uiImage = UIImage(data: imageData) {
-            Image(uiImage: uiImage)
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .clipped()
-        } else {
-            Image(systemName: "photo")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .foregroundColor(.gray)
-        }
-    }
-}
-
-extension Collection {
-    subscript(safe index: Index) -> Element? {
-        return indices.contains(index) ? self[index] : nil
-    }
-}
-
-//MARK: WeatherIconView
-private struct WeatherIconView: View {
-    var iconUrl: String
-    var size: CGFloat
-    
-    fileprivate var body: some View {
-        AsyncImage(url: URL(string: iconUrl)) { phase in
-            switch phase {
-            case .empty:
-                // Placeholder while loading
-                ProgressView()
-                    .frame(width: size, height: size)
-            case .success(let image):
-                // Display the fetched image
-                image
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: size, height: size)
-            case .failure:
-                // Display an error image or icon
-                ProgressView()
-                    .frame(width: size, height: size)
-                //                Image(systemName: "exclamationmark.triangle")
-                //                    .resizable()
-                //                    .scaledToFit()
-                //                    .frame(width: size, height: size)
-                //                    .foregroundColor(.red)
-            @unknown default:
-                // Fallback case
-                Image(systemName: "questionmark")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: size, height: size)
-                    .foregroundColor(.gray)
-            }
-        }
-    }
-}
-
-
 
 #Preview {
     HomeView()
